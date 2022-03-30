936d1e8a185b48d0ca2693a498ae915f5066b08b
Stiltskinner <100236575+Stiltskinner@users.noreply.github.com>
2022-03-30 16:58:28 -0500

    copied over homework files from UTA repo

---
+++ Develop/script.js
@@ -0,0 +1,20 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var test = "sample";
+
+var  generatePassword = function() {
+  document.getElementById
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
